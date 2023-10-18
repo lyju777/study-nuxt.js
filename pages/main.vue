a7a5649b00b7b5906f3021fa0f5aad78f2f5e23f
lyju777 <lyju777@hanmail.net>
2023-10-18 15:59:28 +0900

    nuxt프로젝트 생성

---
+++ pages/main.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>메인 페이지</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LearnNuxtMainJs',
+
+  data() {
+    return {}
+  },
+
+  mounted() {},
+
+  methods: {},
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/product.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>상품 페이지</div>
+</template>
+
+<script>
+export default {
+  name: 'LearnNuxtProduct',
+
+  data() {
+    return {}
+  },
+
+  mounted() {},
+
+  methods: {},
+}
+</script>
+
+<style lang="scss" scoped></style>
