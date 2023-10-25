a87640297f025df3c3fbfd4b04691193776631ee
lyju777 <lyju777@hanmail.net>
2023-10-25 11:42:21 +0900

    메타정보 추가 변경

--- pages/detail/_id.vue
+++ pages/detail/_id.vue
@@ -36,15 +36,17 @@ export default {
   data() {
     return {}
   },
-  head: {
-    title: 'Shopping Item Detail',
-    meta: [
-      {
-        hid: 'description',
-        name: 'description',
-        content: '이 상품은 ~~입니다.',
-      },
-    ],
+  head() {
+    return {
+      title: `Shopping Item Detail - ${this.product.name}`,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: `이 상품은 ${this.product.name}입니다.`,
+        },
+      ],
+    }
   },
 
   //   created() {
