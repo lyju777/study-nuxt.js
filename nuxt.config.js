b9a25b46b7576ac56a6e795f12d679ad52bd017f
lyju777 <lyju777@hanmail.net>
2023-10-25 10:58:02 +0900

    메타 태그 변경

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,14 +2,18 @@ export default {
   target: 'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'learn-nuxt',
+    title: 'Nuxt Shopping',
     htmlAttrs: {
       lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Nuxt Shopping Example',
+      },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
